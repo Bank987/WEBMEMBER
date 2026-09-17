19e3941717495fd4311d314f81e8a26c2e745a6e
Administrator <admin@ganglist.site>
2026-09-18 03:37:49 +0700

    chore: cleanup tmp scripts

--- fix_vip4.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const file = 'src/app/admin/vip/page.tsx';
-let code = fs.readFileSync(file, 'utf8');
-
-code = code.replace(/\n\s*\);\n\}/, '\n    </div>\n  );\n}');
-
-fs.writeFileSync(file, code);
